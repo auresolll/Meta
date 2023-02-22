38cf0d361e1613785688059ec76b33fb535fb35d
auresolll <usool.203@gmail.com>
2023-02-22 15:42:51 +0700

    update: updating validation form

--- packages/dashboard/js/dom.js
+++ packages/dashboard/js/dom.js
@@ -1,4 +1,4 @@
 export const $ = document.querySelector.bind(document);
-export const buttonSignUpWithLocal = $("button__sign__up");
-export const inputFelidUsernameSignUpWithLocal = $("input__sign__up");
-export const inputFelidPasswordSignUpWithLocal = $("input__sign__up");
+export const buttonSignUpWithLocal = $(".button__sign__up");
+export const inputFelidUsernameSignUpWithLocal = $(".input__sign__up");
+export const inputFelidPasswordSignUpWithLocal = $(".input__sign__up");
