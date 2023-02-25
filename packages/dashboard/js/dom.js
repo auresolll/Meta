cade94f96e1f5b01f0c6406618376b3ced7e728e
TranHauTruong <kandytv1@gmail.com>
2023-02-25 20:08:04 +0700

    update animation form UI

--- packages/dashboard/js/dom.js
+++ packages/dashboard/js/dom.js
@@ -2,3 +2,7 @@ export const $ = document.querySelector.bind(document);
 export const buttonSignUpWithLocal = $(".button__sign__up");
 export const inputFelidUsernameSignUpWithLocal = $(".input__sign__up");
 export const inputFelidPasswordSignUpWithLocal = $(".input__sign__up");
+export const buttonChangeFormSignIn = $(".sign__in__button");
+export const buttonChangeFormRegister = $('.sign__up__button');
+export const formSignIn = $(".form_sign_in");
+export const formRegister = $(".form_register");
--- packages/dashboard/js/events.js
+++ packages/dashboard/js/events.js
@@ -1,5 +1,9 @@
 import {
+  buttonChangeFormRegister,
+  buttonChangeFormSignIn,
   buttonSignUpWithLocal,
+  formRegister,
+  formSignIn,
   inputFelidPasswordSignUpWithLocal,
   inputFelidUsernameSignUpWithLocal,
 } from "./dom.js";
@@ -16,11 +20,33 @@ const events = (() => {
     console.log(`[events] => handleSignInClick => loginData:`, loginData);
     authentication.signIn(loginData);
   };
+
+  const handleChangeFormClick = (event, form) => {
+    event.preventDefault ();
+    if (form === true) {
+      formSignIn.classList.remove('show-form')
+      formSignIn.classList.add('display-form')
+      formRegister.classList.remove('display-form')
+      formRegister.classList.add('show-form')
+
+    } else {
+      formSignIn.classList.add('show-form')
+      formSignIn.classList.remove('display-form')
+      formRegister.classList.remove('show-form')
+      formRegister.classList.add('display-form')
+    }
+  }
+
+
   return {
     handleSignInClick,
+    handleChangeFormClick
   };
 })();
 
 // buttonSignUpWithLocal.addEventListener("click", (e) =>
 //   events.handleSignInClick(e)
 // );
+
+buttonChangeFormSignIn.addEventListener("click", (e) => events.handleChangeFormClick(e, false))
+buttonChangeFormRegister.addEventListener("click", (e) => events.handleChangeFormClick(e, true))
